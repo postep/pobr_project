dffa24ce52d865f6f7f589c72137c1cdf815cd06
postep <jakub.postepski@gmail.com>
2018-05-18 13:51:10 +0200

    hsv desaturation

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,12 +1,30 @@
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 #include <iostream>
 #include <math.h>
 
+void hsv_desaturation(cv::Mat &I){
+    cv::Mat_<cv::Vec3b> _I = I;
+    for(int r = 0; r < I.rows; ++r){
+        for(int c = 0; c < I.cols; ++c){
+            _I(r, c)[1] = 0;
+        }
+    }
+
+    I = _I;
+}
+
+
+
 int recognition(cv::Mat I, cv::Mat &R){
     R = I.clone();
     cv::Mat_<cv::Vec3b> _I = I;
+    cv::Mat HSV;
+    cvtColor(I, HSV, CV_BGR2HSV);
+    hsv_desaturation(HSV);
+    cvtColor(HSV, R, CV_HSV2BGR);
     return 0;
 }
 
